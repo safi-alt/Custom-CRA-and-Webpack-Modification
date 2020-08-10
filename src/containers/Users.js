feb191d97d05cd86fe05b4850c85f2045e996e26
safi <safiazim8@gmail.com>
2020-08-10 23:06:33 +0500

    Users Container strcutured

---
+++ src/containers/Users.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class Users extends Component {
+  state = {};
+  render() {
+    return (
+      <div>
+        <h1>The Users</h1>
+        <p>Awesome users on board on my pizza shop</p>
+      </div>
+    );
+  }
+}
+
+export default Users;
