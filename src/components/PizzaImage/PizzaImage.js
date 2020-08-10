6b6d3b41ed9078555c1bc72dd291a0913e7711b1
safi <safiazim8@gmail.com>
2020-08-10 22:54:18 +0500

    Pizza Image component strcutured

---
+++ src/components/PizzaImage/PizzaImage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import classes from "./PizzaImage.css";
+import PizzaImage from "../../assets/pizza.jpg";
+
+const pizzaImage = (props) => {
+  return (
+    <div className={classes.PizzaImage}>
+      <img src={PizzaImage} className={classes.PizzaImage} />
+    </div>
+  );
+};
+
+export default pizzaImage;
