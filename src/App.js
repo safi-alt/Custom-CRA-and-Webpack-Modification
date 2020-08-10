aabec6ed400a1816107b41b8e529220b045186c0
safi <safiazim8@gmail.com>
2020-08-10 23:21:42 +0500

    App.js Container strcutured

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+import { Link, Route } from "react-router-dom";
+
+import Users from "./containers/Users";
+import Pizza from "./containers/Pizza";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <div>
+          <Link to="/">Users</Link> |<Link to="/pizza">Pizza</Link>
+        </div>
+        <div>
+          <Route path="/" exact component={Users} />
+          <Route path="/pizza" component={Users} />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
