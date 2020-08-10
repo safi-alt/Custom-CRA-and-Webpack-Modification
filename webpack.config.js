8096921929ce3822647897470c4831f6863bca9a
safi <safiazim8@gmail.com>
2020-08-11 01:28:18 +0500

    Loading Css images configuration in webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -42,6 +42,10 @@ module.exports = {
           },
         ],
       },
+      {
+        test: /\.(png|jpe?g|gif)$/,
+        loader: "url-loader?limit=8000&name=images/[name].[ext]",
+      },
     ],
   },
 };
