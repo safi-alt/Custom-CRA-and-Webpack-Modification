2b6ca61f480026cf1bf05beab8ce4253862cf34e
safi <safiazim8@gmail.com>
2020-08-10 22:55:18 +0500

    Pizza Image component classname bug fixed

--- src/components/PizzaImage/PizzaImage.js
+++ src/components/PizzaImage/PizzaImage.js
@@ -5,7 +5,7 @@ import PizzaImage from "../../assets/pizza.jpg";
 const pizzaImage = (props) => {
   return (
     <div className={classes.PizzaImage}>
-      <img src={PizzaImage} className={classes.PizzaImage} />
+      <img src={PizzaImage} className={classes.PizzaImg} />
     </div>
   );
 };
