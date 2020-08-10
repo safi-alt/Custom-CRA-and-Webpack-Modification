dded4e5b88071290d13e53ef4fcde83b392d98b3
safi <safiazim8@gmail.com>
2020-08-11 00:59:36 +0500

    Adding webpack config file rules and Babel

--- webpack.config.js
+++ webpack.config.js
@@ -9,4 +9,13 @@ module.exports = {
     publicPath: "",
   },
   devtool: "cheap-module-eval-source-map",
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: "babel-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
 };
